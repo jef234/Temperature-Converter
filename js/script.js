8a557691da88a067dc1e81ca0c7f4edc50b58b94
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-06-03 08:41:09 +0530

    V1.4 ids name change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    $("#txtFahrenheit").bind('input', function () {
+    $("#Fahrenheit").bind('input', function () {
         if (this.value !== "") {
             document.querySelector("#txtCelsius").value = (this.value - 32) * 5 / 9;
         }
     });
 
-    $("#txtCelsius").bind('input', function () {
+    $("#Celsius").bind('input', function () {
         if (this.value !== "") {
             document.querySelector("#txtFahrenheit").value = this.value * 9 / 5 + 32;
         }
